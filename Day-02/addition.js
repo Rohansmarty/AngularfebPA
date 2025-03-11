277ca96cac583a24fed655df6346e322e19d7699
shamanthularohan <shamanthularohan2212@gmail.com>
2025-03-11 13:37:44 -0400

    2nd day

---
+++ Day-02/addition.js
@@ -0,0 +1,14 @@
+/*let a=10;
+let b=20;
+console.log(`Addition of ${a} and ${b} is ${a+b}`);
+
+//window.alert(`Addition of ${a} and ${b} is ${a+b}`);
+//document.write(`Addition of ${a} and ${b} is ${a+b}`);
+/* console.log() document.write() window.alert()
+cosole is object and log is function
+while accessing window object is not necessary
+log(),error(),assert(),table(),trace()time(),timeEnd(),dir()
+window alert(),prompt(),comfirm()
+document write()
+math=max(),min(),sqrt()
+*/
